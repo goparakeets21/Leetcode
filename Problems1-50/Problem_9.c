79f5da44e5d556fe2aa46aff7f9f18625ec609cc
Daniel Olbrys <olbrys.daniel@gmail.com>
2017-07-06 01:12:40 -0400

    Solution to Problem 9

---
+++ Problems1-50/Problem_9.c
@@ -0,0 +1,12 @@
+#include "stdlib.h"
+#include "stdbool.h"
+
+bool isPalindrome(int x) {
+    int sum = 0;
+    if(x<0 || (x!=0 && x%10==0)) return false;
+    while(x > sum) {
+        sum = (sum*10) + (x%10);
+        x = (x/10);
+    }
+    return (x==sum) || (x==sum/10);
+}

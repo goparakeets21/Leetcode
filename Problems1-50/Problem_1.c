3e41ba35f6a09f7026bc2bf394752f4b9c2af9ca
Olbrys <uidj3801@contiwan.com>
2017-07-06 09:40:41 -0400

    Update Problem 1

--- Problems1-50/Problem_1.c
+++ Problems1-50/Problem_1.c
@@ -1,12 +1,13 @@
 #include "stdlib.h"
 
 int* twoSum(int* nums, int numsSize, int target) {
+	int i;
+	int j;
 	int * retVal = (int*)malloc(2 * sizeof(int));
 
-	for (int i = 0; i < numsSize; ++i)
+	for (i = 0; i < numsSize; ++i)
 	{
-		int j = i + 1;
-		for (int j = i + 1; j < numsSize; ++j) {
+		for (j = i + 1; j < numsSize; ++j) {
 			if ((nums[i] + nums[j]) == target) {
 				retVal[0] = i;
 				retVal[1] = j;
